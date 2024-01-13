bab27312d97e7cb60611c71dbad856fc67a10cdf
Angela H <333tripleaxle333@gmail.com>
2024-01-13 17:57:24 -0500

    added result cards

--- website/views.py
+++ website/views.py
@@ -5,3 +5,8 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template("home.html")
+
+@views.route('/results')
+def test():
+    results = [["Pie",["sugar","idk"],["bake it"],"getbootstrap.com/docs/4.0/components/card/#titles-text-and-links"],["Fire Noodles",["noodles","sauce"],["boil water and add noodles","add sauce"],""]]
+    return render_template("results.html",display=results)
