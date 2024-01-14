33bced6cbdfad4454b2129e7ffbb70b5f05db64b
Angela H <333tripleaxle333@gmail.com>
2024-01-14 02:40:52 -0500

    fixing results in progress

--- website/views.py
+++ website/views.py
@@ -17,6 +17,16 @@ def test():
             ingredients = ast.literal_eval(ingredients)
         
         # get array of dics from db
-        recipes = supabase.table("recipe2").select('*').contains('NER', ingredients).execute() # return dish object(s) based on ingredients
-        print(recipes)
-        return render_template("home.html",display=recipes)
+        ingr = supabase.table("recipe2").select('*').contains('NER', ingredients).execute().data
+        print(ingr)
+        results = []
+        for i in ingr:
+            rec = []
+            rec.append(i["name"])
+            rec.append(i["ingredients"])
+            rec.append(i["directions"])
+            rec.append(i["link"])
+            rec.append(i["id"])
+            results.append(rec)
+        print(results)
+        return render_template("results.html",recipes=results)
