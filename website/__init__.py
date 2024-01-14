6ad06c9a6afea38553455e5128ff0a92db20ed56
Angela H <333tripleaxle333@gmail.com>
2024-01-14 01:56:42 -0500

    removed duplicate csv and integrated supabase

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,9 @@
 from flask import Flask
+from supabase import create_client, Client
+
+url = "https://icvzwfgjdjyzvogalfsr.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imljdnp3ZmdqZGp5enZvZ2FsZnNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDUxNjY3MTksImV4cCI6MjAyMDc0MjcxOX0.RaiHts72Fap8jRjr5Lra0RsCsc9WtIm7KBaYAXMMbGY"
+supabase: Client = create_client(url, key)
 
 def create_app():
     app = Flask(__name__)
--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, flash
+from src import supabase
 
 views = Blueprint('views', __name__)
 
@@ -10,7 +11,16 @@ def home():
         if ingredients:
             # need recipes variable that checks data base for recipes with all ingredients
             # recipes = ["queried recipes"]
-            recipes = [["Pie",["sugar","idk"],["bake it"],"getbootstrap.com/docs/4.0/components/card/#titles-text-and-links"],["Fire Noodles",["noodles","sauce"],["boil water and add noodles","add sauce"],""]]
+            ingr = supabase.table("recipe2").select('*').contains('NER', ingredients).execute().data
+            recipes = []
+            for i in ingr:
+                rec = []
+                rec.append(i["name"])
+                rec.append(i["ingredients"])
+                rec.append(i["directions"])
+                rec.append(i["link"])
+                rec.append(i["id"])
+                recipes.append(rec)
 
             # Send recipe list to the page
             # Could also save ingredients through session; could be glitchy due to nature of flask
