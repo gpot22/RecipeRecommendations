a0492581de89805378efa97c96e2ec0b3f36f8eb
BambooShi <110565810+BambooShi@users.noreply.github.com>
2024-01-13 22:01:57 -0500

    established connection with db

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,8 @@
-import os
-from supabase import create_client, Client, ClientOptions
+from supabase import create_client, Client
 
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
-supabase: Client = create_client(url, key,
-  options=ClientOptions(
-    postgrest_client_timeout=10,
-    storage_client_timeout=10
-  ))
+url = "https://icvzwfgjdjyzvogalfsr.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imljdnp3ZmdqZGp5enZvZ2FsZnNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDUxNjY3MTksImV4cCI6MjAyMDc0MjcxOX0.RaiHts72Fap8jRjr5Lra0RsCsc9WtIm7KBaYAXMMbGY"
+supabase: Client = create_client(url, key)
+
+response = supabase.table("recipes").select("*").execute()
+print(response)
